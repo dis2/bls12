e97628645ad3f002aede3f88422cecc4ff9d04fe
dis <dis@firemail.cc>
2018-03-11 15:33:32 +0100

    F(r) ops

--- relic_amd64.go
+++ relic_amd64.go
@@ -2,3 +2,4 @@ package bls12
 
 // #cgo CFLAGS: -std=c99 -O2 -I. -DARCH=X64 -DWORD=64 -Irelic/include -Irelic/src -Irelic/include/low -Wno-unused -DALLOC=AUTO -Wno-discarded-qualifiers
 import "C"
+
